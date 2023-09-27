4c4f840c60b9c90a8795dc82cf9039f83215aae9
ejani <ejani@veracode.com>
2023-09-27 17:07:02 +0200

    fix: availability zone index

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 locals {
   public_cidr = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
   private_cidr = ["10.0.100.0/24", "10.0.101.0/24", "10.0.102.0/24"]
-  availability_zones = ["eu-central-1a", "eu-central-1b"]
+  availability_zones = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
 }
 
 resource "aws_vpc" "my_vpc" {
